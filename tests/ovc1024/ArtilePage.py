41f99374d1c6d72d387252b9acef89ab56b971a6
zhangjun95 <497296792@qq.com>
2019-07-13 18:28:10 +0800

    zhangjun

---
+++ tests/ovc1024/ArtilePage.py
@@ -0,0 +1,28 @@
+class ArtilePage():
+
+    url="http://47.92.220.226:8000/bbs2/"
+
+    def __init__(self,driver):
+        self.driver=driver
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def register(self,info):
+
+        self.element_email_input.send_keys(info[u'email'])
+        self.element_password_input.send_keys(info[u'password'])
+        element_submit = self.driver.find_element_by_xpath(u'//*[@id="comm-submit"]')
+        element_submit.click()
+
+    @property
+    def element_email_input(self):
+        element= self.driver.find_element_by_xpath(u"//div[3]/div/div[2]/div/div/form/div[1]/input")
+        return element
+
+    @property
+    def element_password_input(self):
+        element = self.driver.find_element_by_xpath(u"//div[3]/div/div[2]/div/div/form/div[2]/input")
+        return element
+
+
